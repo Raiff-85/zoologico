41bba6e57217e1a7d16d3cdc1d6067cedb557b57
Raiff-85 <emanoel.gomes@dcx.ufpb.br>
2024-03-07 22:52:26 -0300

    inicial

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('Olá, Mundo!');
+    }
+    #[Route('/dinossauro')]
+    public function dinossauro(): Response
+    {
+        return new Response('Olá, Dino!');
+    }
+}
